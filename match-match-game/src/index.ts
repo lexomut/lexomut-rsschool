bf0a86d9ebc3f03b1d9f8a6562a7bad276c3bba4
lexomut <lexomut@mail.ru>
2021-05-10 16:51:42 +0300

    feat:install css-loader

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -1,2 +1,3 @@
-import { test } from  './test';
-test();
+import './style.scss'
+const img  =require("./assets/images/map.jpg");
+document.body.innerHTML=`<img src="${img}" alt="map">`
--- match-match-game/webpack.config.js
+++ match-match-game/webpack.config.js
@@ -1,52 +1,141 @@
 const path = require('path');
-const HtmlWebpackPlugin=require('html-webpack-plugin')
-const CopyPlugin=require('copy-webpack-plugin')
-const {CleanWebpackPlugin}=require('clean-webpack-plugin')
-// const MiniCssExtractPlugin= require('mini-css-extract-plugin')
-module.exports = {
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        overlay: true,
+        open: true,
+        hot: true,
+        port: 8080,
+        contentBase: path.join(__dirname, 'public'),
+    },
+};
+
+module.exports = ({ development }) => ({
+    mode: development ? 'development' : 'production',
+    devtool: development ? 'inline-source-map' : false,
     entry: './src/index.ts',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        assetModuleFilename:'assets/[hash][ext]',
+        assetModuleFilename: 'assets/[hash][ext]',
     },
     module: {
-        rules:[
+        rules: [
             {
-                test:/\.[tj]s$/,
-                use:'ts-loader',
-                exclude: /node-modules/,
+                test: /\.[tj]s$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
                 type: 'asset/resource',
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                test: /\.(woff(2)?|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
-            // {
-            //     test: /\.s[ac]ss$/i,
-            //     use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader", "sass-loader",'sass-loader']
-            // },
-        ]
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ],
     },
     resolve: {
-        extensions:['.ts','js']
+        extensions: ['.ts', 'js']
     },
-    plugins:[
-      new HtmlWebpackPlugin({
-          // title:'match-match-game',
-          template:'./src/index.html'
-      }),
-        // new MiniCssExtractPlugin({
-        //     filename:'[name].[contenthash].css',
-        // }),
+    plugins: [
+        new HtmlWebpackPlugin({
+            // title:'match-match-game',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
 
         new CopyPlugin({
             patterns: [{from: './public'}],
-            }, ),
+        },),
 
-        new CleanWebpackPlugin({cleanStaleWebpackAssets:false})
+        new CleanWebpackPlugin({cleanStaleWebpackAssets: false})
     ],
-};
+    ...devServer(development)
+});
+
+
+//
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
+// // const ESLintPlugin = require('eslint-webpack-plugin');
+//
+// const devServer = (isDev) => !isDev ? {} : {
+//     devServer: {
+//         open: true,
+//         hot: true,
+//         port: 8080,
+//         contentBase: path.join(__dirname, 'public'),
+//     },
+// };
+//
+// const esLintPlugin = (isDev) => isDev ? [] : [ new ESLintPlugin({ extensions: ['ts', 'js'] }) ];
+//
+// module.exports = ({ development }) => ({
+//     // mode: development ? 'development' : 'production',
+//     // devtool: development ? 'inline-source-map' : false,
+//     entry: {
+//         main: './src/index.ts',
+//     },
+//     output: {
+//         filename: '[name].[contenthash].js',
+//         path: path.resolve(__dirname, 'dist'),
+//         assetModuleFilename: 'assets/[hash][ext]',
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.[tj]s$/,
+//                 use: 'ts-loader',
+//                 exclude: /node_modules/,
+//             },
+//             {
+//                 test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+//                 type: 'asset/resource',
+//             },
+//             {
+//                 test: /\.(woff(2)?|eot|ttf|otf)$/i,
+//                 type: 'asset/resource',
+//             },
+//             {
+//                 test: /\.css$/i,
+//                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
+//             },
+//             {
+//                 test: /\.s[ac]ss$/i,
+//                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+//             }
+//         ],
+//     },
+//     plugins: [
+//         // ...esLintPlugin(development),
+//         new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+//         new HtmlWebpackPlugin({ title: 'The Cantina' }),
+//         new CopyPlugin({
+//             patterns: [
+//                 { from: 'public' },
+//             ],
+//         }),
+//         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+//     ]
+//     // resolve: {
+//     //     extensions: ['.ts', '.js'],
+// //     },
+// //     // ...devServer(development)
+//  });
